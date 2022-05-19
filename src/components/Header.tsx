4506a20a2dbe57c03dcafe83a15427b35d9ce271
Tomoki <nowvilla.physi.311@gmail.com>
2022-05-19 23:05:23 +0900

    ロゴを追加した。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,23 @@
-import styles from '../styles/header.module.scss';
+import Image from 'next/image';
+import styles from '../styles/Header.module.scss';
+import { SearchForm } from './index';
+import logo from '../../public/images/logo.png';
+
+const toTop = () => {
+    console.log('###');
+};
 
 function Header() {
     return (
-        <header className={styles.header} />
+        <header className={styles.header}>
+            <h1 className={styles.header__logo}>
+                <Image src={logo} alt='Flutter School' onClick={toTop} />
+            </h1>
+            <SearchForm />
+            <h1 className={styles.header__logo}>
+                <Image src={logo} alt='Flutter School' onClick={toTop} />
+            </h1>
+        </header>
     );
 }
 
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,8 +1,21 @@
-import styles from '../styles/header.module.scss';
+import styles from '../styles/SearchForm.module.scss';
+import * as Strings from '../constants/strings';
 
 function SearchForm() {
     return (
-        <section  />
+        <form action=''>
+            <div className={styles['search-form']}>
+                <input
+                    className={styles['search-form__text']}
+                    placeholder={Strings.SEARCH_FORM_PLACEHOLDER}
+                />
+                <input
+                    type='submit'
+                    value={Strings.SEARCH_FORM_BUTTON}
+                    className={styles['search-form__button']}
+                />
+            </div>
+        </form>
     );
 }
 
