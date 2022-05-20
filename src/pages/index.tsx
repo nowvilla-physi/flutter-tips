3a9e7893a52bff5c766322d39f02f6c2e812dac9
Tomoki <nowvilla.physi.311@gmail.com>
2022-05-20 22:22:36 +0900

    画面を分割した。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,25 @@ import Link from 'next/link';
 import { InferGetStaticPropsType } from 'next';
 import client from '../../lib/client';
 import { Blog } from '../models/blog';
+import styles from '../styles/Home.module.scss';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 function Home({ blogs }: Props) {
     return (
-        <div>
-            <ul>
-                {blogs.map((blog: Blog) => (
-                    <li key={blog.id}>
-                        <Link href={`/blog/${blog.id}`}>
-                            <a>{blog.title}</a>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+        <div className={styles.home}>
+            <section className={styles.home__navigation} />
+            <section className={styles.home__contents}>
+                <ul>
+                    {blogs.map((blog: Blog) => (
+                        <li key={blog.id}>
+                            <Link href={`/blog/${blog.id}`}>
+                                <a>{blog.title}</a>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </div>
     );
 }
