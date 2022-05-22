cddc462e9a8426404f6f33882a31cff1373ffde3
Tomoki <nowvilla.physi.311@gmail.com>
2022-05-22 14:17:57 +0900

    formを使用しないようにした。

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,21 +1,30 @@
+import { useState } from 'react';
 import styles from '../styles/SearchForm.module.scss';
 import * as Strings from '../constants/strings';
 
 function SearchForm() {
+    const [text, setText] = useState('');
+
+    const search = () => {
+        console.log(text);
+    };
+
     return (
-        <form action=''>
-            <div className={styles['search-form']}>
-                <input
-                    className={styles['search-form__text']}
-                    placeholder={Strings.SEARCH_FORM_PLACEHOLDER}
-                />
-                <input
-                    type='submit'
-                    value={Strings.SEARCH_FORM_BUTTON}
-                    className={styles['search-form__button']}
-                />
-            </div>
-        </form>
+        <div className={styles['search-form']}>
+            <input
+                className={styles['search-form__text']}
+                placeholder={Strings.SEARCH_FORM_PLACEHOLDER}
+                value={text}
+                onChange={(event) => setText(event.target.value)}
+            />
+            <button
+                type='button'
+                className={styles['search-form__button']}
+                onClick={search}
+            >
+                {Strings.SEARCH_FORM_BUTTON}
+            </button>
+        </div>
     );
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from 'next/app';
 import NextNProgress from 'nextjs-progressbar';
+import Head from 'next/head';
 import { Footer, Header } from '../components/index';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <div className='app'>
+            <Head>
+                <title>FlutterTips</title>
+            </Head>
             <NextNProgress
                 color='#53C6F8'
                 startPosition={0.2}
