8b0251daaeaec1bb3b7aab4848e79c7705ef0cf6
Tomoki <nowvilla.physi.311@gmail.com>
2022-05-22 13:05:29 +0900

    プログレスバーを表示するようにした。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import { Footer, Header } from '../components/index';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <div className='app'>
+            <NextNProgress
+                color='#53C6F8'
+                startPosition={0.2}
+                stopDelayMs={50}
+                height={3}
+                showOnShallow
+            />
             <Header />
             <Component {...pageProps} />
             <Footer />
